b3059e7dfe4cfc36468733c82580feab6dddd127
jupemo <otojupe@gmail.com>
2020-04-24 23:19:26 +0200

    product.service 'httpResponse'

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit{
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = true;
+  errorMessage = '';
   
   constructor(private productService: ProductService){
   }
@@ -41,8 +42,13 @@ export class ProductListComponent implements OnInit{
   }
  
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products; 
+    this.productService.getProducts().subscribe({
+    next: products => {
+      this.products = products;
+      this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
   }
 
   onRatingClicked(message: string): void {
